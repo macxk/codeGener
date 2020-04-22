500b58f45cb59433ae70347e8bcc6c1e8be24073
macxk <2396655699@qq.com>
2020-04-22 23:38:34 +0800

    fix

--- src/dbutil.js
+++ src/dbutil.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 exports.createConn = () => {
     return mysql.createConnection({
-        host: '192.168.0.219',
-        user: 'developer',
-        password: 'sunvua@developer',
-        database: 'crrc_tdms_dev',
+        host: '',
+        user: '',
+        password: '',
+        database: '',
         port: 3306
     });
 };
